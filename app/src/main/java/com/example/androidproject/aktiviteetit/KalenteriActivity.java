36b80709b4a77c76abbddadd12a9019a7f63c6a8
AOskari <oskari.arponen@hotmail.fi>
2021-02-18 19:34:33 +0200

    Kommentointu ja korjattu varoitukset TiedotActivitystä ja KalenteriActivitystä.

--- app/src/main/java/com/example/androidproject/aktiviteetit/KalenteriActivity.java
+++ app/src/main/java/com/example/androidproject/aktiviteetit/KalenteriActivity.java
@@ -17,6 +17,10 @@ import android.widget.TextView;
 import com.example.androidproject.R;
 import com.google.gson.Gson;
 
+/**
+ * Luo popup-ikkunan, joka näyttää kalenterin.
+ * Päivämäärän voi asettaa painamalla kalenterissa päivämäärää.
+ */
 public class KalenteriActivity extends AppCompatActivity {
 
     SharedPreferences pref;
@@ -26,34 +30,34 @@ public class KalenteriActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kalenteri);
+        getSupportActionBar().hide();
 
         pref = getApplicationContext().getSharedPreferences("mainPref",0);
         editor = pref.edit();
 
+        /**
+         * Asetetaan popup-ikkunan koko 80% näytön leveydestä ja 40% korkeudesta.
+         * Asetetaan taustaväri läpinäkyväksi, joka tuo esille popup-ikkunan pyöristetyt reunat.
+         */
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
-
-        // Piilotetaan yläpalkki.
-        getSupportActionBar().hide();
-
         int width = metrics.widthPixels;
         int height = metrics.heightPixels;
-
-        // Asetetaan popup-ikkunan koko 60% näytön koosta.
         getWindow().setLayout((int) (width * .6), (int) (height * 0.4));
         getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
+        /**
+         * Asetetaan kalenteriin kuuntelija, joka muuttaa valinnan perusteella päivämäärää
+         * ja lopuksi poistaa kalenterin näkyvistä.
+         */
         CalendarView kalenteri = findViewById(R.id.kalenteri);
-
         kalenteri.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-
                 editor.putInt("vuosi", year);
                 editor.putInt("kuukausi", month);
                 editor.putInt("paiva", dayOfMonth);
                 editor.commit();
-
                 finish();
             }
         });
