145ef9747ac5af7a36f51bedd9c9d33cc4e1c73c
timoh <timojuhani.hypponen@gmail.com>
2021-02-24 11:43:03 +0200

    etusivun juttujasss

--- app/src/main/java/com/example/androidproject/aktiviteetit/MainActivity.java
+++ app/src/main/java/com/example/androidproject/aktiviteetit/MainActivity.java
@@ -8,6 +8,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.transition.Fade;
 import android.transition.Transition;
@@ -22,9 +23,15 @@ import android.widget.TextView;
 import com.example.androidproject.Ateria;
 import com.example.androidproject.AteriaLista;
 import com.example.androidproject.R;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import static com.example.androidproject.AteriaLista.haeLista;
@@ -56,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
          * Haetaan AteriaLista-singleton pysyväismuistista.
          * Jos singletonia ei löydy, luodaan uusi instanssi ja tallennetaan se pysyväismuistiin.
          */
-        pref = getApplicationContext().getSharedPreferences("mainPref",0);
+        pref = getApplicationContext().getSharedPreferences("mainPref", 0);
         editor = pref.edit();
         aterialistaJson = pref.getString("aterialista", "");
 
@@ -97,9 +104,10 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences sharedPreferences = getSharedPreferences("Tiedot", Context.MODE_PRIVATE);
         String nimi = sharedPreferences.getString("Käyttäjä", "");
         String tiedot1 = sharedPreferences.getString("Tavoite1", "");
+        String tiedot2 = sharedPreferences.getString("Tavoite2", "");
 
         //Päivän tähän asti syödyt kalorit verrattuna omaan tavoitteeseen.
-       // String jokuvitunhienolaskujonkateenmyöhemmin = "";
+        // String jokuvitunhienolaskujonkateenmyöhemmin = "";
 
         /**
          * Asetetaan tiedot, mikäli tietoja on tallennettu asetuksissa.
@@ -107,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
         if (!tiedot1.equals("")) {
             String[] tiedot1_lista = tiedot1.split(",");
             String tiedot1_kalorit = tiedot1_lista[1];
-           int kaloritYht = Integer.parseInt(tiedot1_kalorit);
+            int kaloritYht = Integer.parseInt(tiedot1_kalorit);
 
             /**
              *     Muutetaan intit doubleiksi laskua varten.
@@ -117,9 +125,10 @@ public class MainActivity extends AppCompatActivity {
             double kY = kaloritYht;
             double prosentit = k / kY * 100;
             int prosentitI = (int) Math.round(prosentit);
+            //  double prosentitD = Math.round(prosentit);
 
             //Muutetaan vastaus Stringiksi ja pyöristetään se.
-            String prosentitS = Double.toString(prosentitI);
+            String prosentitS = Integer.toString(prosentitI);
 
             //Jäljellä olevat kalorit.
             int jaljella = kaloritYht - kalorit;
@@ -130,7 +139,7 @@ public class MainActivity extends AppCompatActivity {
 
             kaloriTavoite.setText("Tavoite päivässä :" + tiedot1_kalorit + " kcal" + "\nKaloreita jäljellä: " + jaljella + " kcal");
 
-            if(prosentitI > 100) {
+            if (prosentitI > 100) {
                 //Kertoo että ylitit päivän kaloritavoitteen.
                 kaloriTavoite.setText("Tavoite päivässä: " + tiedot1_kalorit + " kcal" + "\nKaloreita jäljellä: " + jaljella + " kcal \nYlitit päivän kaloritavoitteen.");
             }
@@ -139,20 +148,63 @@ public class MainActivity extends AppCompatActivity {
             prossat.setText(prosentitS + " %");
 
             //Prosenttipalkki.
-            ProgressBar simpleProgressBar=(ProgressBar)findViewById(R.id.progressBar); // initiate the progress bar
-            //int progressValue=simpleProgressBar.getProgress();
+            ProgressBar simpleProgressBar = (ProgressBar) findViewById(R.id.progressBar); // initiate the progress bar
             simpleProgressBar.setProgress(prosentitI);
+
+            PieChart chart = (PieChart) findViewById(R.id.progressBar2);
+            chart.animateXY(2000, 2000);
+
+          //  PieData data = new PieData();
+            ArrayList lista = new ArrayList();
+            ArrayList kaloritL = new ArrayList();
+            float kS = (float) kalorit;
+            float kYht = (float) kaloritYht;
+       //     float m3 = (float) ras;
+            lista.add(new Entry(kS, 0));
+            lista.add(new Entry(kYht, 1));
+        //    lista.add(new Entry(m3, 2));
+            PieDataSet dataSet = new PieDataSet(lista, "");
+
+            kaloritL.add("Syödyt kalorit");
+            kaloritL.add("Kalorit yhteensä");
+        //    kaloritL.add("rasva");
+
+            PieData data = new PieData(kaloritL, dataSet);
+
+
+            /**
+             * Asetetaan uudet ravintoarvot diagrammiin, poistetaan ylimääräiset merkinnät
+             * ja asetetaan värit.
+             */
+
+            data.setDrawValues(false);
+            chart.setData(data);
+            chart.setDescription("");
+            chart.getLegend().setEnabled(false);
+            chart.setDrawSliceText(false);
+
+       //     GradientDrawable dataSet;
+            dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+
+
+        }
+
+        //Toisen itseasetetun tavoitteen näyttäminen.
+        if (!tiedot2.equals("")) {
+            String[] tiedot2_lista = tiedot2.split(",");
+            String tiedot2_2 = tiedot2_lista[1];
+            String tiedot2_1 = tiedot2_lista[0];
 
             //Lisätiedot, proteiini, hiilarit, rasva
             TextView lisatiedot = findViewById(R.id.lisatiedot);
-            lisatiedot.setText("koodiakoodiakoodia...");
+            lisatiedot.setGravity(Gravity.CENTER);
+            lisatiedot.setText("Oma tavoite, \n" + tiedot2_1 + ": " + tiedot2_2 + " g/vrk");
         }
-
         /**
          * Asetetaan nimi, mikäli nimi on tallennettu asetuksissa.
          * Varmistetaan että nimessä on aina iso alkukirjain laatikossa näytettäessä,
          * vaikka käyttäjä kirjoittaisi nimensä pienellä.
-        */
+         */
         if (!nimi.equals("")) {
             TextView nimitextview = findViewById(R.id.nimiteksti);
             String isoalkukirjain = nimi.substring(0, 1).toUpperCase() + nimi.substring(1);
