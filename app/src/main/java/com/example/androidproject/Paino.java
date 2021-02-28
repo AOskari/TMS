4ad2dadd0d990bf4bbf89c180e6248ed80a949b9
miisa <miisa.ekholm@gmail.com>
2021-02-28 22:22:27 +0200

    Profiilinäkymän muutokset

---
+++ app/src/main/java/com/example/androidproject/Paino.java
@@ -0,0 +1,11 @@
+package com.example.androidproject;
+
+public class Paino {
+    private float arvo;
+    public Paino(float arvo) {
+        this.arvo = arvo;
+    }
+    public String toString(){
+        return String.valueOf(this.arvo);
+    }
+}
