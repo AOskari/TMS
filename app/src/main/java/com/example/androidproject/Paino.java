f6a31eea59f75625c73e6701647b87449995bb5b
AOskari <oskari.arponen@hotmail.fi>
2021-03-02 10:28:40 +0200

    Lisätty Paino-luokkaan päivämäärän asetus ja metodi päivämäärän hakuun.

--- app/src/main/java/com/example/androidproject/Paino.java
+++ app/src/main/java/com/example/androidproject/Paino.java
@@ -1,10 +1,30 @@
 package com.example.androidproject;
 
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
 public class Paino {
+
     private float arvo;
+    private int paiva;
+    private int kuukausi;
+    private int vuosi;
+
     public Paino(float arvo) {
         this.arvo = arvo;
+
+        Calendar kalenteri = Calendar.getInstance();
+        paiva = kalenteri.get(Calendar.DAY_OF_MONTH);
+        kuukausi = kalenteri.get(Calendar.MONTH);
+        vuosi = kalenteri.get(Calendar.YEAR);
     }
+
+    public List<Integer> haePainonPaivamaara() {
+        List<Integer> palautettavat = Arrays.asList(paiva, kuukausi, vuosi);
+        return palautettavat;
+    }
+
     public String toString(){
         return String.valueOf(this.arvo);
     }
