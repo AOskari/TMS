149e5e2f13339bcffb30566d5792a90f309d4a7a
timoh <timojuhani.hypponen@gmail.com>
2021-03-08 17:15:14 +0200

    no katotaanpas

--- app/src/main/java/com/example/androidproject/aktiviteetit/MainActivity.java
+++ app/src/main/java/com/example/androidproject/aktiviteetit/MainActivity.java
@@ -726,6 +726,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickStreakki(View view) {
 
-        Toast.makeText(this, "Näyttää kuinka monta päivää putkeen olet käyttänyt sovellusta.", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Näyttää kuinka monta päivää putkeen olet käyttänyt sovellusta. Hyvä sinä!", Toast.LENGTH_SHORT).show();
     }
 }
